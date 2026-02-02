221da410c79903deafd7a47b084440fdde974337
daslu <daniel.slutsky@gmail.com>
2026-02-02 21:07:44 +0200

    updated notebooks workflow

--- notebooks/dev.clj
+++ notebooks/dev.clj
@@ -1,13 +1,26 @@
 (ns dev
   (:require [scicloj.clay.v2.api :as clay]))
 
-(clay/make! {:format [:quarto :html]
-             :base-source-path "notebooks"
-             :source-path (->> "notebooks/chapters.edn"
-                               slurp
-                               clojure.edn/read-string
-                               (map #(format "pocket_book/%s.clj" %))
-                               (cons "index.clj"))
-             :base-target-path "docs"
-             :book {:title "Pocket"}
-             :clean-up-target-dir true})
+(comment
+  ;; render book HTML through Quarto
+  (clay/make! {:format [:quarto :html]
+               :base-source-path "notebooks"
+               :source-path (->> "notebooks/chapters.edn"
+                                 slurp
+                                 clojure.edn/read-string
+                                 (map #(format "pocket_book/%s.clj" %))
+                                 (cons "index.clj"))
+               :base-target-path "docs"
+               :book {:title "Pocket"}
+               :clean-up-target-dir true})
+
+  ;; render all as github flavored markdown
+  (clay/make! {:format [:gfm]
+               :base-source-path "notebooks"
+               :source-path (->> "notebooks/chapters.edn"
+                                 slurp
+                                 clojure.edn/read-string
+                                 (map #(format "pocket_book/%s.clj" %))
+                                 (cons "index.clj"))
+               :base-target-path "gfm"
+               :show false}))
